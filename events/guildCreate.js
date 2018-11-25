cb015e6286a352b4df9bc89b88f9d56dff027cef
Pikachu <40127806+Pikachu-Director@users.noreply.github.com>
2018-11-24 17:43:50 -0800

    Update and rename guildAdd.js to guildCreate.js

---
+++ events/guildCreate.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+module.exports.run = (bot, guild) => {
+    require('../util/playing.js')(bot)
+  let embed = new Discord.RichEmbed()
+    .setColor(`#FF000`)
+    .setDescription(`${guild.name}**[\`[${guild.id}]\`](${guild.iconURL ? guild.iconURL : "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png"})**\nServer Created: **${guild.createdAt.toString().substr(0, 15)},\n${checkDays(guild.createdAt)}**\nWe now have **${bot.guilds.size}** Servers`)
+    .setAuthor(`Server Joined`, guild.iconURL ? guild.iconURL : "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png")
+    .setTimestamp()
+    bot.channels.get("516065058539700265").send(embed)
+}
