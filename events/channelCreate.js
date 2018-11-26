14b24ba4da00052c99be4e1dd6887498fb1584b1
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2018-11-25 20:40:02 -0500

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, channel) => {
     if (channel.type === "dm") return undefined;
     let kerlogs = channel.guild.channels.find(c => c.name === "kerlogs");
-    if (!modlogs) return;
+    if (!kerlogs) return;
     let botembed = new Discord.RichEmbed()
         .setColor("#FF000")
         .setAuthor('Channel Created', channel.guild.iconURL)
         .setFooter(`ID: ${channel.id}`)
         .setTimestamp()
         .setDescription(`_ _►Name<#${channel.id}> (**${channel.name}**) \n ►Type **${channel.type}** \n ►ID **${channel.id}**`)
-    await modlogs.send(botembed);
+    await kerlogs.send(botembed);
 }
