0ac42927a8b2366b1c294f8df06468fa7e8a21b0
Pikachu <40127806+Pikachu-Director@users.noreply.github.com>
2018-11-25 18:16:23 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,7 @@ module.exports.run = (client) => {
         status: "Online",
         game: {
          name: "For The Doctor To Return",
-            type: "Watching", "Playing",  
+         type: "WATCHING" // Types: WATCHING, PLAYING, LISTENING  
        }
     })
 
