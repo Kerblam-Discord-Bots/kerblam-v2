2724365dffe4fadc38f901eb8a76b53bb8ee1ecc
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2018-11-25 19:57:12 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -11,6 +11,7 @@ module.exports.run = (client) => {
         status: "online",
         game: {
             name: "With Delivery",
+                  "kb!help for more info"
             type: "PLAYING"
         }
     })
