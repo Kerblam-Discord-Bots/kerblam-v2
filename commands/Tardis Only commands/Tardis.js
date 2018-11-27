493e6b7a4a7c8bcfaf17186c66c58ff2b923036d
Pikachu <40127806+Pikachu-Director@users.noreply.github.com>
2018-11-26 16:17:40 -0800

    Update Tardis.js

--- commands/Tardis Only commands/Tardis.js
+++ commands/Tardis Only commands/Tardis.js
@@ -7,15 +7,17 @@ module.exports = class NCommand extends Command {
             memberName: "regenerarte", // this is the same as the name above ^ 
             aliases: ["regen"], // Add any aliases to this command, like [`name here`, `other name here`] etc
             examples: ["kb!regen"], // Give a example of how to do the command 
-            description: "This explains about regeneration"
+            description: "This explains about regeneration",
             group: "tardis"
         })
     }
     async run(message) {
-      // Add the code here for the bot to run when the command is called. 
-    } let replies = ["Regeneration for TimeAnimals is changing into new animals"]
+         let replies = ["Regeneration for TimeAnimals is changing into new animals"]
          let result = Math.floor((Math.random() * replies.length));
-         let question = content
          let regenembed = new Discord.RichEmbed()
             .setAuthor(message.author.tag, message.author.displayAvatarURL)
             .setColor("#000FF")
+            .setDescription(replies[result])
+         message.channel.send(regenembed)
+    }
+} 
