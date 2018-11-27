7324b7c4718fa374780c5db5bafaa675e68d8eca
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2018-11-26 19:08:20 -0500

    Update Tardis.js

--- commands/Tardis Only commands/Tardis.js
+++ commands/Tardis Only commands/Tardis.js
@@ -1,1 +1,21 @@
-// Code here
+const {Command} = require('discord.js-commando'),
+ Discord = require('discord.js')
+module.exports = class NCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "regenerate", // the command name goes here
+            memberName: "regenerarte", // this is the same as the name above ^ 
+            aliases: ["regen"], // Add any aliases to this command, like [`name here`, `other name here`] etc
+            examples: ["kb!regen"], // Give a example of how to do the command 
+            description: "This explains about regeneration"
+            group: "tardis"
+        })
+    }
+    async run(message) {
+      // Add the code here for the bot to run when the command is called. 
+    } let replies = ["Regeneration for TimeAnimals is changing into new animals"]
+         let result = Math.floor((Math.random() * replies.length));
+         let question = content
+         let regenembed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL)
+            .setColor("#000FF")
