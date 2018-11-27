daeada50da29db26eebf7214ba87ace027fb0358
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2018-11-26 19:37:13 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports.run = (client, member) => {
-    let modlogs = member.guild.channels.find(c => c.name === "modlogs");
-    if (!modlogs) return;
+    let kerlogs = member.guild.channels.find(c => c.name === "kerlogs");
+    if (!kerlogs) return;
     let botembed = new Discord.RichEmbed()
         .setColor("#1CFF00")
         .setAuthor('Member Joined', member.user.displayAvatarURL)
@@ -9,5 +9,5 @@ module.exports.run = (client, member) => {
         .setTimestamp()
         .setDescription(`${member} ${member.user.tag}`)
         .setThumbnail(member.user.displayAvatarURL)
-    modlogs.send(botembed);
+    kerlogs.send(botembed);
 }
