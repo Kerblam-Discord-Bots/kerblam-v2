d215ac9df7f116d729d5774786deaa4be2b9bde3
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2018-11-26 19:43:06 -0500

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 module.exports.run = (bot, message) => {
     if (message.channel.type === "dm") return;
     if (message.author.bot) return;
-    let modlogs = message.guild.channels.find(c => c.name === "modlogs") || message.guild.channels.find(c => c.name === "🤖modlogs")
-    if (!modlogs) return;
+    let modlogs = message.guild.channels.find(c => c.name === "kerlogs") || message.guild.channels.find(c => c.name === "kerlogs")
+    if (!kerlogs) return;
     let image = message.attachments.map(g => g.proxyURL)
     if (message.content === 0) return;
     let botembed = new Discord.RichEmbed()
@@ -12,5 +12,5 @@ module.exports.run = (bot, message) => {
         .setAuthor(`Message Deleted By ${message.author.tag}`, `${message.author.displayAvatarURL}`)
         .setFooter(`${bot.user.tag}`, `${bot.user.displayAvatarURL}`)
         .setDescription(`_ _►Content: **${message.content ? message.content : image}** \n ►Channel: <#${message.channel.id}> \n ►Message ID: ${message.id}`)
-    modlogs.send(botembed)
+    kerlogs.send(botembed)
 }
