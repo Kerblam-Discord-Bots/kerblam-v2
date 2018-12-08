d59d0716e89609dd9b6fa30fdcbffa42d90667c3
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2018-12-08 15:52:31 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -16,3 +16,8 @@ module.exports.run = (client) => {
     })
 
 }
+let embed = new Discord.RichEmbed()
+    .setColor(`#FF000`)
+    .setTimestamp()
+    .setDescription(`${client.user} has connected`)
+    client.channels.get("521066410345103360").send(embed)
