94ca9e6a19022b03b197a4a1b1ee3fa85a6443aa
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2019-01-13 18:06:16 -0500

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const {arrayClean} = require('../util/util.js');
 module.exports.run = (bot, oldMember, newMember) => {
-    let modlogs = oldMember.guild.channels.find(c => c.name === "modlogs");
-    if (!modlogs) return;
+    let kerlogs = oldMember.guild.channels.find(c => c.name === "modlogs");
+    if (!kerlogs) return;
     if (oldMember.nickname !== newMember.nickname) {
         let embed = new Discord.RichEmbed()
             .setColor(`RANDOM`)
@@ -12,7 +12,7 @@ module.exports.run = (bot, oldMember, newMember) => {
             .addField(`Old Nickname`, `${oldMember.nickname ? `${oldMember.nickname}` : `${oldMember.user.username}`}`)
             .addField(`New Nickname`, `${newMember.nickname ? `${newMember.nickname}` : `${newMember.user.username}`}`)
             .setTimestamp()
-        modlogs.send(embed)
+        kerlogs.send(embed)
     } else
         if (oldMember.roles !== newMember.roles) {
             let role1 = oldMember.roles.size > 1 ? arrayClean(null, oldMember.roles.map((r) => { if (r.name !== '@everyone') { return r; } return null; })).join(' | ') : 'None'
@@ -25,6 +25,6 @@ module.exports.run = (bot, oldMember, newMember) => {
                 .addField(`Old Roles`, `${role1}`)
                 .addField(`New Roles`, `${role2}`)
                 .setTimestamp()
-            modlogs.send(embed)
+            kerlogs.send(embed)
         }
 }
