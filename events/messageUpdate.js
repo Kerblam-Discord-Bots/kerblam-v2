0b44f9217443f2da2ec45339d64b0b4761bc1b44
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2019-01-13 18:07:30 -0500

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 module.exports.run = (bot, oldMessage, newMessage) => {
     if (oldMessage.channel.type === "dm") return;
     if (newMessage.author.bot) return;
-    let modlogs = oldMessage.guild.channels.find(c => c.name === "modlogs");
-    if (!modlogs) return;
+    let kerlogs = oldMessage.guild.channels.find(c => c.name === "kerlogs");
+    if (!kerlogs) return;
     let content = oldMessage.content;
     if (content.length === 0) return;
     let content2 = newMessage.content;
@@ -17,12 +17,12 @@ module.exports.run = (bot, oldMessage, newMessage) => {
           .setDescription(`${content}`)
           .setAuthor(`Message Updated`, oldMessage.author.displayAvatarURL)
           .addField(`Info`, `**User:** ${oldMessage.author.tag}\n**User ID:** ${oldMessage.author.id}\n**Channel:** ${oldMessage.channel}\n**Channel ID:** ${oldMessage.channel.id}\nNew Message will be down below below :arrow_double_down:`)
-      modlogs.send(embed)
+      kerlogs.send(embed)
       let embed2 = new Discord.RichEmbed()
       .setColor(`#FF0000`)
       .setTitle(`New Message`)
       .setDescription(content2)
-      modlogs.send(embed2)
+      kerlogs.send(embed2)
   }else 
   if(length < 2040){
       let embed = new Discord.RichEmbed()
@@ -31,6 +31,6 @@ module.exports.run = (bot, oldMessage, newMessage) => {
           .setDescription(`**Old Message: **\n${content}\n\n**New Message: **\n${content2}`)
           .setAuthor(`Message Updated`, oldMessage.author.displayAvatarURL)
           .addField(`Info`, `**User:** ${oldMessage.author.tag}\n**User ID:** ${oldMessage.author.id}\n**Channel:** ${oldMessage.channel}\n**Channel ID:** ${oldMessage.channel.id}`)
-      modlogs.send(embed)
+      kerlogs.send(embed)
   }
 }
