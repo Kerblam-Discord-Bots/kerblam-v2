6b9cada17418193f56447eeae0380135f1c7a042
The78thdoctor <45296988+The78thdoctor@users.noreply.github.com>
2019-01-21 17:31:49 -0500

    Update kerblamit.js

--- commands/Fun/kerblamit.js
+++ commands/Fun/kerblamit.js
@@ -6,22 +6,28 @@ module.exports = class NCommand extends Command {
             name: "kerblamit",
             memberName: "kerblamit",
             aliases: [],
-            examples: ["e!throw @user/userid"],
+            examples: [`${client.commandPrefix}throw @user/userid`],
             description: "Throw something at the user",
             group: "fun",
-            ownerOnly: true,
             args: [
                 {
                     key: "user",
-                    prompt: "What user do you want to throw something at?",
+                    prompt: "What do you want Kerblam to give you?",
                     type: "user"
+                },
+                {
+                    key: "object",
+                    prompt: "What do you want Kerblam to give you",
+                    type: "string",
+                    default: "random"
                 }
             ]
         })
     }
-    async run(message, {user}) {
-        const throwuser = user
-        let replies = [`Banana`, `Car`, `Truck`, `Daleks`, `A Building`,
+    async run(message, { user, object }) {
+        try{
+        if(object.toLowerCase() === "random"){
+           let replies = [`Banana`, `Car`, `Truck`, `Daleks`, `A Building`,
             `SpongeBob`, 'Patrick', 'Nothing', 'Admins', 'Moderators', 'Staff Members',
             'Black Hole', 'Scams', `Love`, `Hate`, `iPhone`, `Brick`, `Bad Bots`, `Chair`,
             `Lemons`, `Cake`, `Sonic Gauntlet`, `Gummy Bears`, `Bus`, `Train`, `Yourself`, `Doc's Glasses`, `UR MOM`,
@@ -31,7 +37,16 @@ module.exports = class NCommand extends Command {
         let result = Math.floor((Math.random() * replies.length));
         let embed = new Discord.RichEmbed()
             .setColor("RANDOM")
-            .setDescription(`Threw **${replies[result]}** at **${throwuser}**`)
+            .setDescription(`Delivered **${replies[result]}** to **${user}**`)
         message.channel.send(embed);
+        }else{
+            let embed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .setDescription(`Delivered **${object}** to **${user}**`)
+            message.channel.send(embed);
+        }
+        } catch (e) {
+          console.log(e.stack)
+        }
     }
 }
