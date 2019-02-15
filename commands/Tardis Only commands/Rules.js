6fa046fb78a7e5fafadd475080e5d094b1afda99
Doctor <45296988+The78thdoctor@users.noreply.github.com>
2019-02-15 14:15:29 -0500

    Update Rules.js

--- commands/Tardis Only commands/Rules.js
+++ commands/Tardis Only commands/Rules.js
@@ -23,7 +23,7 @@ module.exports = class StaffCommand extends Command {
             .addBlankField()
             .addField("Tardis Rules", "1. Do not agrue with the Tardis staff or Doctor   2. No cursing in the tardis incuding Voice chat's  3. Listen to and work with staffs, not against them.  4. No text spamming and cussing in the text channels  5. No mic spamming and cussing in the voice channels.  6. Trolling and bullying is not allowed in any form in The Tardis.  7. No alternate identities or accounts   8. No asking Doctor to be Kerblam owner")
             .addBlankField()
-            .addField("AutoBan", "1. Not listening to staff after being warned Alot   2. Pornographic, disturbing content, or racism.  3. Links to Porn or Racism Sites disturbing stuff   4. Impersonate another human or Bot.   5. Snipping In the Discord   6. No Drama From Other Servers, after being told to stop and not doing so.")
+            .addField("AutoBan", "1. Not listening to staff after being warned Alot   2. Pornographic, disturbing content, or racism.  3. Links to Porn or Racism Sites disturbing stuff   4. Impersonate another human or Bot.   5. Snipping In the Discord   6. No Drama From Other Servers, after being told to stop and not doing so.  7. Bullying The Bear Doctor")
             .addBlankField()
             .addField("Playing Status and Profile Photo And Usernames", "Shouldn't have Cursing or nothing to bad or a Tardis Staff will contact you to change it")
             .addBlankField()
