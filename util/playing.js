873798523504cea50b63a7ff212f35417f43f05a
Doctor <45296988+The78thdoctor@users.noreply.github.com>
2019-02-15 13:52:47 -0500

    Update playing.js

--- util/playing.js
+++ util/playing.js
@@ -1,11 +1,11 @@
 module.exports = (bot) => {
-    bot.user.setActivity(`Kerblam is always watching`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
+    bot.user.setActivity(`Kerblam is always watching`, { type: "PLAYING", url: "https://www.twitch.tv/elarabots_discord" });
     var status = [
         `If you want it, just Kerblam it`
     ];
     setInterval(() => {
         let gameval = Math.floor((Math.random() * status.length));
-        bot.user.setActivity(`${status[gameval]}`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
+        bot.user.setActivity(`${status[gameval]}`, { type: "LISTENING", url: "https://www.twitch.tv/elarabots_discord" });
     }, 10 * 1000);
 }
 // This is to set your bot's playing status it will randomize through the statuses
