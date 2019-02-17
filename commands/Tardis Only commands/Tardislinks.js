d312233533ca1270e9f08e0c091e92c9f8aae128
Doctor <45296988+The78thdoctor@users.noreply.github.com>
2019-02-17 15:06:55 -0500

    Update Tardislinks.js

--- commands/Tardis Only commands/Tardislinks.js
+++ commands/Tardis Only commands/Tardislinks.js
@@ -21,7 +21,7 @@ module.exports = class NCommand extends Command {
             .addField(`Twitter`, `[Click Here](https://twitter.com/ImReallyCoco)`, true)
             .addField(`Instagram`, `[Click Here](https://www.instagram.com/imactuallycoco/)`, true)
             .addField(`Discord`, `**${this.client.users.get('288491554841034752').tag}**`, true)
-            .addField(`Discord Server`, `[Click Here](https://discord.gg/xSJ55Et)`, true)
+            .addField(`Discord Server`, `[Click Here](https://discord.gg/NeM886f)`, true)
             .addField(`Reddit`, `[Click Here](https://www.reddit.com/r/ImReallyCoco/?st=JS7PQRY6&sh=1087efb9)`,true)
         message.channel.send(embed)
     }
