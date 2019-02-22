9e4102aecec30989930bdce72f45374c703214d7
Doctor <45296988+The78thdoctor@users.noreply.github.com>
2019-02-22 17:58:07 -0500

    Update Rules.js

--- commands/Tardis Only commands/Rules.js
+++ commands/Tardis Only commands/Rules.js
@@ -17,12 +17,10 @@ module.exports = class StaffCommand extends Command {
         let botembed = new Discord.RichEmbed()
            .setDescription("The Rules Of The Tardis")
         .setColor("#000FF")
-        .setThumbnail(bicon)
         .addField("General Rules", "1. Do not agrue with the Tardis Staff or The Doctor\n2. No cursing in the Tardis incuding Voice chat's\n3. Listen to and work with staffs, not against them.\n4. No text spamming and cussing in the text channels\n5. No mic spamming and cussing in the voice channels.\n6. Trolling and bullying is not allowed in any form.\n7. No self bots")
         .addBlankField()
         .addField("AutoBan", "1. Not listening to staff after being warned Alot\n2. Pornographic, disturbing content, or racism.\n3. Links to Porn or Racism Sites disturbing stuff\n4. Impersonate another person or Bot.\n5. Snipping In the Discord\n6. No Drama From Other Servers, after being told to stop and not doing so.")
         .addBlankField()
         .addField("FOR EMERGENCYS ONLY", "In Case of an Emergency if someone is Cussing or sending bad photos links etc. Please tag <@&490168027049099285>")
-        .setImage("https://cdn.discordapp.com/attachments/444028025932349441/445612424545042453/tenor_1.gif")
     message.channel.send(botembed);
     message.delete().catch();
