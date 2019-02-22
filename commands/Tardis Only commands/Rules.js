c24760ed2f549071f0d2485a545ce8ee24cd3dee
Doctor <45296988+The78thdoctor@users.noreply.github.com>
2019-02-22 17:27:18 -0500

    Update Rules.js

--- commands/Tardis Only commands/Rules.js
+++ commands/Tardis Only commands/Rules.js
@@ -1,4 +1,4 @@
-const { Command } = require('discord.js-commando'),
+const { Command } = require('Discord.js-Commando'),
     Discord = require('discord.js');
 module.exports = class StaffCommand extends Command {
     constructor(client) {
@@ -10,24 +10,21 @@ module.exports = class StaffCommand extends Command {
             guildOnly: true,
             ownerOnly: true,
             examples: [`${client.commandPrefix}rules`],
-            description: "Posts the rules for Coco's Discord."
+            description: "Posts the rules for The Tardis."
         })
     }
     async run(message) {
-        let bicon = ("https://cdn.discordapp.com/attachments/529525290528145409/539197734058459147/image0.jpg");
+        let bicon = ("https://cdn.discordapp.com/attachments/444028025932349441/445611500246269972/alert.gif");
         let botembed = new Discord.RichEmbed()
-            .setDescription("The Rules Of The Tardis and info")
+            .setDescription("The Rules Of The Tardis")
             .setColor("#000FF")
             .setThumbnail(bicon)
-            .addField("Info", "The Tardis has a mind of its own please do NOT call it a Server")
+            .addField("Main Rules", "1. Do not agrue with the Staff or Doctor himself                                                                           2. No cursing in the Tardis incuding Voice chat's                                                   3. Listen to and work with staffs, not against them.                                                      4. No text spamming and cussing in the text channels                                                      5. No mic spamming and cussing in the voice channels.                             6. Trolling and bullying is not allowed in any form even if you're joking                                                                          7. No alternate identities or accounts                                                                  8. No self bots")
             .addBlankField()
-            .addField("Tardis Rules", "1. Do not agrue with the Tardis staff or Doctor                              2. No cursing in the tardis incuding Voice chat's                    3. Listen to and work with staffs, not against them.                     4. No text spamming and cussing in the text channels                     5. No mic spamming and cussing in the voice channels.                     6. Trolling and bullying is not allowed in any form in The Tardis.                      7. No alternate identities or accounts                       8. No asking Doctor to be Kerblam owner")
-            .addBlankField()
-            .addField("AutoBan", "1. Not listening to staff after being warned Alot                                                         2. Pornographic, disturbing content, or racism.                                           3. Links to Porn or Racism Sites disturbing stuff                                        4. Impersonate another human or Bot.                      5. Snipping In the Discord                                6. No Drama From Other Servers, after being told to stop and not doing so.                                7. Bullying The Bear Doctor")
-            .addBlankField()
-            .addField("Playing Status and Profile Photo And Usernames", "Shouldn't have Cursing or nothing to bad or a Tardis Staff will contact you to change it")
-            .addBlankField()
-            .addField("Coco's IRL Friends", "if you are Coco's IRL Friends and your new to discord. you have no right to criticize Coco or The Tardis itself and no right to break the rules")
+            .addField("AutoBan", "1. Not listening to Tardis staff after being warned Alot                                                      2. Pornographic, disturbing content, or racism.                                        3. Links to Porn or Racism Sites disturbing stuff                                                                                4. Impersonate another person or Bot.                                                                  5. Snipping In the Discord                                                                                                                            6. No Drama From Other Servers, after being told to stop and not doing so.")
+            .addField("FOR EMERGENCYS ONLY", "In Case of an Emergency if someone is Cussing or sending bad photos links etc. Please tag <@&490168027049099285>")
+            .setImage("https://cdn.discordapp.com/attachments/444028025932349441/445612424545042453/tenor_1.gif")
+            .addField("if you're Friends with The Doctor in real life in his school or not, The Doctor/Coco will NOT allow you to say his Real name or allow you to break the Rules. We care for his safety")
         message.channel.send(botembed);
         message.delete().catch();
     }
