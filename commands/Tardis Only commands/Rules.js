59df8487b5933b7e73f1996746f74716eb657498
kerblamdiscordbots <45296988+The78thdoctor@users.noreply.github.com>
2019-02-24 18:25:26 -0500

    Update Rules.js

--- commands/Tardis Only commands/Rules.js
+++ commands/Tardis Only commands/Rules.js
@@ -19,7 +19,7 @@ module.exports = class StaffCommand extends Command {
         .setColor("#000FF")
         .addField("General Rules", "1. Do not agrue with the Tardis Staff or The Doctor\n2. No cursing in the Tardis incuding Voice chat's\n3. Listen to and work with staffs, not against them.\n4. No text spamming and cussing in the text channels\n5. No mic spamming and cussing in the voice channels.\n6. Trolling and bullying is not allowed in any form.\n7. No self bots.\n8. Don't dm The Doctor he is a busy bear.\n9. Exposure of The Doctor is Prohibited We know he is human.\n10. Please keep bot comamnds in <#546859083437703173> ")
         .addBlankField()
-        .addField("AutoBan", "1. Not listening to staff after being warned Alot\n2. Pornographic, disturbing content, or racism.\n3. Links to Porn or Racism Sites disturbing stuff\n4. Impersonate another person or Bot.\n5. Snipping In the Discord\n6. No Drama From Other Servers, after being told to stop and not doing so.\n7. Gay Slurs ")
+        .addField("AutoBan", "1. Not listening to staff after being warned Alot\n2. Pornographic, disturbing content, or racism.\n3. Links to Porn or Racism Sites disturbing stuff\n4. Impersonate another person or Bot.\n5. Snipping In the Discord\n6. No Drama From Other Servers, after being told to stop and not doing so.\n7. Gay Slurs\n8. Racist and Disrespectful memes")
         .addBlankField()
         .addField("FOR EMERGENCYS ONLY", "In Case of an Emergency if someone is Cussing or sending bad photos links etc. Please tag <@&490168027049099285>")
     message.channel.send(botembed);
