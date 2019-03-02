a0e2192c80bf03cef2e9e9fbdaf9bd925c3e9238
Kerblam! <47952197+Kerblam-Discord-Bots@users.noreply.github.com>
2019-03-02 10:47:00 -0500

    Update botinfo.js

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -27,11 +27,11 @@ module.exports = class BotinfoCommand extends Command {
             .addField(`ID`, this.client.user.id, true)
             .addField(`Discriminator`, `#${this.client.user.discriminator}`, true)
             .addField(`Bot Owner(s)`, this.client.owners, true)
-            .addField(`Developer(s)`, this.client.developer, true)
             .addField("Created On", `**${this.client.user.createdAt}**`, true)
             .addField("Invite Link", `[Click Here](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&permissions=8&scope=bot)`, true)
             .addField("Twitter", `[Click Here](https://twitter.com/kerblamdiscord)`, true)
-            .addField("TARDIS",  `[Click Here](https://discord.gg/xSJ55Et)`, true)
+            .addField("TARDIS",  `[Click Here](https://discord.gg/NeM886f)`, true)
+            .addField("SUPPORT SERVER", `[Click Here(https://discord.gg/cmymsAY)`, true)
             .setFooter(`Requested By ${message.author.tag}`, message.author.displayAvatarURL);
         message.say(botembed)
     }
