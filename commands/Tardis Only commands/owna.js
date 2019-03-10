6f3c55021047b3ae4222bd0784608cce81a4550d
Kerblam! <47952197+Kerblam-Discord-Bots@users.noreply.github.com>
2019-03-10 15:28:23 -0400

    Create owna.js

---
+++ commands/Tardis Only commands/owna.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
+
+module.exports = class SayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'owna',
+            group: 'botowner',
+            memberName: 'owna',
+            description: 'Sends a message to the channel',
+            aliases: ["embed"],
+            examples: [`${client.commandPrefix}announce #channel Hi there!`],
+            ownerOnly: true,
+            userPermissions: ["MANAGE_MESSAGES"],
+            args: [
+                {
+                    key: "channel",
+                    prompt: "What channel do you want me to post the announcement to?",
+                    type: "channel"
+                },
+                {
+                    key: 'content',
+                    prompt: 'What would you like the content of the message to be?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    async  run(msg, {channel, content }) {
+        let embed = new Discord.RichEmbed()
+        .setColor(`RANDOM`)
+        .setDescription(content)
+        channel.send(embed)
+        msg.delete().catch()
+    }
+};
