a4ea73c13c49960f2322e608cc26baed9f861d74
Kerblam! <47952197+Kerblam-Discord-Bots@users.noreply.github.com>
2019-03-18 18:48:10 -0400

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,7 +9,7 @@ module.exports.run = (client, member) => {
         .setTimestamp()
         .setDescription(`${member} ${member.user.tag}`)
         .setThumbnail(member.user.displayAvatarURL)
-let tardiswelcome = member.guild.channels.find(c => c.name === "tardiswelcome");
-if(tardiswelcome) return channel.send(`"${member.user.username}" has joined the TARDIS`)
+let welcometothetardis = member.guild.channels.find(c => c.name === "welcometothetardis");
+if(welcometothetardis) return channel.send(`"${member.user.username}" has joined the TARDIS`)
     kerlogs.send(botembed);
 }
