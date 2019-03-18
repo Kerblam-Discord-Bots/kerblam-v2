2c8160650203156febbd31835f816be9e4fe4f4e
Kerblam! <47952197+Kerblam-Discord-Bots@users.noreply.github.com>
2019-03-18 18:59:00 -0400

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 module.exports.run = (client, member) => {
-    let kerlogs = member.guild.channels.find(c => c.name === "kerlogs");
-    if (!kerlogs) return;
+    let logs= member.guild.channels.find(c => c.name === "kerlogs");
     let botembed = new Discord.RichEmbed()
         .setColor("#1CFF00")
         .setAuthor('Member Joined', member.user.displayAvatarURL)
@@ -9,7 +8,7 @@ module.exports.run = (client, member) => {
         .setTimestamp()
         .setDescription(`${member} ${member.user.tag}`)
         .setThumbnail(member.user.displayAvatarURL)
-let welcometothetardis = member.guild.channels.find(c => c.name === "welcometothetardis");
-if(welcometothetardis) return channel.send(`"${member.user.username}" has joined the TARDIS`)
-    kerlogs.send(botembed);
+    if(logs) { logs.send(botembed);}
+let channel = member.guild.channels.find(c => c.name === "welcome");
+if(channel) return channel.send(`**${member.user.username}** has joined the server!`)
 }
