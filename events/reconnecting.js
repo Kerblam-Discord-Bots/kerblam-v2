8f733c12afd4b19a8c7a61d9ce764f764e925860
Kerblam! <47952197+Kerblam-Discord-Bots@users.noreply.github.com>
2019-03-24 20:13:16 -0400

    Update reconnecting.js

--- events/reconnecting.js
+++ events/reconnecting.js
@@ -1,9 +1,16 @@
-module.exports.run = (bot) => {
-    bot.user.setPresence({
-        status: "online",
-        game: {
-            name: "The Tardis!",
-            type: "PLAYING"
-        }
-    })
+const {RichEmbed} = require('discord.js');
+module.exports.run = async (client) => {
+    let e = new RichEmbed()
+    .setAuthor(client.user.tag, client.user.displayAvatarURL)
+    .setDescription(`Reconnecting`)
+    .setTimestamp()
+    .setColor(client.util.colors.yellow)
+    client.channels.get(client.config.log).send(e)
+    let embed = new RichEmbed()
+    .setAuthor(client.user.tag, client.user.displayAvatarURL)
+    .setColor(client.util.colors.yellow)
+    .setTitle(`Reconnecting`)
+    .setTimestamp()
+    client.channels.get("521023970007449610").send(embed)
 }
+
